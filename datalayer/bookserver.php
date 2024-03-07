9df3f0c04be1f3cde3e36121e5aa389dd4be63d3
nishanthkj <nishanthkj9@gmail.com>
2024-03-08 02:36:41 +0530

    main

--- datalayer/bookserver.php
+++ datalayer/bookserver.php
@@ -3,7 +3,7 @@
 $errors=array();
 include ('server.php');
 
-$mysqli = new mysqli("localhost","root","","registration");
+$mysqli = new mysqli("localhost","root","","sourcecodester_doctordb");
 
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
