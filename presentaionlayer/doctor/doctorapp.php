6269d41e0d168070410b689247595720e172b5ba
nishanthkj <nishanthkj9@gmail.com>
2024-03-08 03:17:59 +0530

    Update doctorapp.php

--- presentaionlayer/doctor/doctorapp.php
+++ presentaionlayer/doctor/doctorapp.php
@@ -1,23 +1,24 @@
-<?php include '../../datalayer/bookserver.php '; ?>
+<?php include '..\..\datalayer\bookserver.php'; ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>Doctor</title>
-	<link rel="stylesheet"  href="style3.css">
+	<link rel="stylesheet" href="style3.css">
 
 	<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
 </head>
 
 <header>
 	<h1>Doctor<span>Patient</span></h1>
-		<nav>
-		
+	<nav>
 
 
-		
-		<ul> 
-			
-		
+
+
+		<ul>
+
+
 			<li><a href="index2.php">My Info</a></li>
 			<li><a href="doctorapp.php">My Appointments</a></li>
 			<li><a href=" searchpatient.php">Search Patient</a></li>
@@ -26,11 +27,11 @@
 
 
 
-	
-			
+
+
 
 		</ul>
-		
+
 
 
 
@@ -46,36 +47,33 @@
 
 	<table class="table2">
 		<tr>
-		<th>Appointment ID</th>
-		<th>DATE</th>
-		<th>TIME</th>
-		<th>PatientID</th>
-		<th>PatientName</th>
-		<th>PatientAddress</th>
-		<th>PatientEmail</th>
-		<th>PatientContactNumber</th>
-		<th>BloodGroup</th>
+			<th>Appointment ID</th>
+			<th>DATE</th>
+			<th>TIME</th>
+			<th>PatientID</th>
+			<th>PatientName</th>
+			<th>PatientAddress</th>
+			<th>PatientEmail</th>
+			<th>PatientContactNumber</th>
+			<th>BloodGroup</th>
 
-		
+
 
 		</tr>
-		<?php $sqldoc="SELECT  * FROM bookapp , patients   WHERE docID=('$doctorprofile') AND  patientID=UserID "  ;
-		$resultdoc=$mysqli->query($sqldoc);
-		if(mysqli_num_rows($resultdoc)>= 1){
-			while ($rowdoc=$resultdoc->fetch_assoc()) {
+		<?php $sqldoc = "SELECT  * FROM bookapp , patients   WHERE docID=('$doctorprofile') AND  patientID=UserID ";
+		$resultdoc = $mysqli->query($sqldoc);
+		if (mysqli_num_rows($resultdoc) >= 1) {
+			while ($rowdoc = $resultdoc->fetch_assoc()) {
 
-				echo "<tr><td>".$rowdoc["AppoID"]."</td><td>".$rowdoc["Date"]."</td><td>".$rowdoc["Time"]."</td><td>".$rowdoc["UserID"]."</td><td>".$rowdoc['Name']."</td><td>".$rowdoc['Address']."</td><td>".$rowdoc['Email']."</td><td>".$rowdoc["ContactNumber"]."</td><td>".$rowdoc["Bloodtype"]."</td></tr>";
+				echo "<tr><td>" . $rowdoc["AppoID"] . "</td><td>" . $rowdoc["Date"] . "</td><td>" . $rowdoc["Time"] . "</td><td>" . $rowdoc["UserID"] . "</td><td>" . $rowdoc['Name'] . "</td><td>" . $rowdoc['Address'] . "</td><td>" . $rowdoc['Email'] . "</td><td>" . $rowdoc["ContactNumber"] . "</td><td>" . $rowdoc["Bloodtype"] . "</td></tr>";
 			}
 
 
 			echo "</table";
-	
-
-
 		}
 
 		?>
-		
+
 	</table>
 
 
@@ -83,5 +81,5 @@
 
 
 </body>
-</html>
 
+</html>
